9a2aa33ab993e17f50c2d0c3e804e5675f0f3de4
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-21 13:24:17 -0300

    .

---
+++ etapa_1/soma_dois_numeros.py
@@ -0,0 +1,11 @@
+# Leia dois números e mostre a soma.
+import os
+os.system('clear')
+
+print('Insira o primeiro número: ')
+n1 = int(input())
+
+print('\nInsira o segundo número: ')
+n2 = int(input())
+
+print(f'{n1} + {n2} = {n1 + n2}')
