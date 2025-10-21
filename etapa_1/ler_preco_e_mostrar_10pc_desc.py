4f602ad72f0e7fcc91980496edc2e62ab9e05020
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-21 13:44:08 -0300

    .

---
+++ etapa_1/ler_preco_e_mostrar_10pc_desc.py
@@ -0,0 +1,9 @@
+# Leia o preço de um produto e mostre com 10% de desconto.
+import os
+os.system('clear')
+
+preco_produto = int(input('Insira o preço do produto: R$'))
+
+desconto_produto = preco_produto * 0.1
+
+print(f'O Produto sairá R${preco_produto - desconto_produto} após a aplicação do desconto')
