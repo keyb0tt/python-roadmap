bfaea619aead8f56917729916efe6aedb128cdfa
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-21 13:34:19 -0300

    .

--- etapa_1/dobro_triplo_e_raiz_quadrada.py
+++ etapa_1/dobro_triplo_e_raiz_quadrada.py
@@ -1,1 +1,13 @@
 # Peça um número e mostre o dobro, triplo e raiz quadrada.
+import os
+os.system('clear')
+
+n1 = int(input('Insira o número desejado: '))
+
+dobro = n1 * 2
+triplo = n1 * 3
+raiz_quadrada = n1 ** 0.5
+
+print(f'{n1} x 2 = {dobro}')
+print(f'{n1} x 3 = {triplo}')
+print(f'Raiz² de {n1} = {raiz_quadrada}')
