4ab0c41e27e93c296f4f53485a72d2f1e965ef18
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-21 13:38:40 -0300

    .

--- etapa_1/metros_para_cm_e_mm.py
+++ etapa_1/metros_para_cm_e_mm.py
@@ -7,5 +7,5 @@ metros_qnt = int(input('Insira uma quantidade em metros: '))
 cm_qnt = metros_qnt * 100
 mm_qnt = metros_qnt * 1000
 
-print(f'Quantidade convertida em centímetros: {cm_qnt}')
-print(f'Quantidade convertida em milímetros: {mm_qnt}')
+print(f'\nQuantidade convertida em centímetros: {cm_qnt}\n'
+      f'Quantidade convertida em milímetros: {mm_qnt}')
