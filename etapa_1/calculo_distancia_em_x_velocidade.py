b1e553d82993786743f3d25df613963cdf056aa0
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-21 14:51:20 -0300

    .

--- etapa_1/calculo_distancia_em_x_velocidade.py
+++ etapa_1/calculo_distancia_em_x_velocidade.py
@@ -4,19 +4,17 @@ os.system('clear')
 
 velocidade_km_h = int(input('Insira a velocidade em km/h do veículo: '))
 distancia_km = int(input('Insira a distância a ser percorrida em km: '))
+tempo_horas, tempo_minutos, tempo_segundos = (0, 0, 0)
 
-tempo_horas = 0
 while distancia_km >= velocidade_km_h:
     tempo_horas += 1
     distancia_km -= velocidade_km_h
 
-tempo_minutos = 0
 velocidade_km_m = velocidade_km_h / 60
 while distancia_km >= velocidade_km_m:
     tempo_minutos += 1
     distancia_km -= velocidade_km_m
 
-tempo_segundos = 0
 velocidade_km_s = velocidade_km_m / 60
 while distancia_km >= velocidade_km_s:
     tempo_segundos += 1
