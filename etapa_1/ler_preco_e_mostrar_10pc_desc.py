e850a626a2e2fc62d677636c86a6652e6055cdbb
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-21 13:44:59 -0300

    .

--- etapa_1/ler_preco_e_mostrar_10pc_desc.py
+++ etapa_1/ler_preco_e_mostrar_10pc_desc.py
@@ -6,4 +6,4 @@ preco_produto = int(input('Insira o preço do produto: R$'))
 
 desconto_produto = preco_produto * 0.1
 
-print(f'O Produto sairá R${preco_produto - desconto_produto} após a aplicação do desconto')
+print(f'O Produto sairá R${(preco_produto - desconto_produto):.2f} após a aplicação do desconto')
