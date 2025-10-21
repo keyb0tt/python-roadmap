a9aef20e94299109437e4273bfb3015b7b470478
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-21 13:40:37 -0300

    .

---
+++ etapa_1/dias_de_vida.py
@@ -0,0 +1,9 @@
+# Peça a idade e mostre quantos dias de vida aproximadamente tem.
+import os
+os.system('clear')
+
+idade = int(input('Insira a sua idade: '))
+
+dias_de_vida = idade * 365
+
+print(f'Você tem aproximadamente {dias_de_vida} dias de vida.')
