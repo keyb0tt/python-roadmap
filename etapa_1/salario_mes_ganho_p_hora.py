d744f52ec85148b83e0615781ecada6943f70f74
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-22 09:33:59 -0300

    .

--- etapa_1/salario_mes_ganho_p_hora.py
+++ etapa_1/salario_mes_ganho_p_hora.py
@@ -2,4 +2,9 @@
 import os
 os.system('clear')
 
-salario_mes = input(int('Insira a sua renda salarial por mês: '))
+salario_mes = int(input('Insira a sua renda salarial por mês: '))
+qnt_horas_mes, qnt_horas_dia = {160, 24}
+salario_dia = salario_mes / (qnt_horas_mes / qnt_horas_dia)
+salario_hora = salario_dia / qnt_horas_dia 
+
+print(f'{salario_hora}')
