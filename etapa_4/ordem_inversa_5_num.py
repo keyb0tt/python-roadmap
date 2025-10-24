a100661219e3b43a7c64e6391b466354e9b3ed7d
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-24 11:45:31 -0300

    .

--- etapa_4/ordem_inversa_5_num.py
+++ etapa_4/ordem_inversa_5_num.py
@@ -8,8 +8,10 @@ array_root = []
 for i in range(array_size):
     array_root.append(int(input(f'Insira o {i + 1}° elemento da array: ')))
 
+# Método convencional (mais parecido com C)
 # for i in range(array_size, 0, -1): 
 #     print(array_root[i - 1])
 
+# Método mais típico do python
 for element in reversed(array_root):
     print(element)
