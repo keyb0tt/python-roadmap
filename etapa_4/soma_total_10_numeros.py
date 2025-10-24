216c58cadbe481134ff413ca3a767ee16380b8f6
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-24 13:25:26 -0300

    .

--- etapa_4/soma_total_10_numeros.py
+++ etapa_4/soma_total_10_numeros.py
@@ -3,7 +3,16 @@ import os
 os.system('clear')
 
 array_numeros = []
+soma_numeros = 0
 qnt_numeros = int(input('Insira a quantidade de números a serem somados: ')) 
+os.system('clear')
 
 for i in range(qnt_numeros):
-    array_numeros.append(int(input(f'Insira o {i+1}° n')))
+    array_numeros.append(int(input(f'Insira o {i+1}° número: ')))
+    os.system('clear')
+
+for numero in array_numeros:
+    soma_numeros += numero
+
+print(f'{array_numeros=}\n')
+print(f'{soma_numeros=}')
