8c60e95e400ca9c1ca4588dcdf784a5dd8b593d0
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-27 08:48:47 -0300

    .

--- etapa_4/dez_num_rand_qnt_pares.py
+++ etapa_4/dez_num_rand_qnt_pares.py
@@ -1,3 +1,18 @@
 # Gere 10 números aleatórios e mostre quantos são pares.
 import os
+import random
 os.system('clear')
+
+numeros = []
+numeros_pares = []
+contagem_pares = 0
+
+for i in range(5):
+    numeros.append(int(random.randint(1, 50)))
+    if numeros[i] % 2 == 0:
+        numeros_pares.append(int(numeros[i]))
+        contagem_pares += 1
+
+print(f'{numeros=}')
+print(f'{numeros_pares=}')
+print(f'{contagem_pares=}')
