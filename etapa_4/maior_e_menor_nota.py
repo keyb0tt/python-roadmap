c23e5e058f16527eae22c7689b6ea55e35e5ca36
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-27 08:35:54 -0300

    .

--- etapa_4/maior_e_menor_nota.py
+++ etapa_4/maior_e_menor_nota.py
@@ -2,3 +2,25 @@
 import os 
 os.system('clear')
 
+notas_1aluno = []
+notas_2aluno = []
+notas_3aluno = []
+
+for aluno in range(3):
+    for i in range(3):
+        print(f'Aluno {aluno + 1}:')
+        if aluno == 0:
+            notas_1aluno.append(int(input(f'Insira a {i+1}ª nota: ')))
+            os.system('clear')
+        elif aluno == 1:
+            notas_2aluno.append(int(input(f'Insira a {i+1}ª nota: ')))
+            os.system('clear')
+        elif aluno == 2:
+            notas_3aluno.append(int(input(f'Insira a {i+1}ª nota: ')))
+            os.system('clear')
+
+notas = [*notas_1aluno, *notas_2aluno, *notas_3aluno]
+maior_nota = max(notas)
+
+print(f'{notas=}')
+print(f'{maior_nota=}')
